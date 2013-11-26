4f11b92ccebafd38906314a47020d916b74d7927
11008880 <11008880@i204m01.istic.univ-rennes1.fr>
2013-11-26 13:38:13 +0100

    last

--- chrono3/src/BO/EvenementSportif.java
+++ chrono3/src/BO/EvenementSportif.java
@@ -8,4 +8,32 @@ public class EvenementSportif {
 	private int longueurCircuit;
 	public ArrayList<Course> courses = new ArrayList<Course>();
 	public ArrayList<Voiture> voitures = new ArrayList<Voiture>();
+	
+	public EvenementSportif(String nom, String nomCircuit, int longueurCircuit, ArrayList<Course> courses, ArrayList<Voiture> voitures){
+		this.nom = nom;
+		this.nomCircuit = nomCircuit;
+		this.longueurCircuit = longueurCircuit;
+		this.courses = courses;
+		this.voitures = voitures;
+	}
+	
+	public String getNom(){
+		return nom;
+	}
+	
+	public String getNomCircuit(){
+		return nomCircuit;
+	}
+	
+	public int getLongueurCircuit(){
+		return longueurCircuit;
+	}
+	
+	public ArrayList<Course> getCourses(){
+		return courses;
+	}
+	
+	public ArrayList<Voiture> getVoitures(){
+		return voitures;
+	}
 }
