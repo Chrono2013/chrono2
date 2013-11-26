cd95ffd3e2354ec7f69cb411f1e5726e8e1d602c
11008880 <11008880@i204m01.istic.univ-rennes1.fr>
2013-11-26 13:54:52 +0100

    aaa

---
+++ chrono3/src/ViewModel/eventViewModel.java
@@ -0,0 +1,15 @@
+package ViewModel;
+
+import java.util.ArrayList;
+import BO.EvenementSportif;
+
+
+
+public class eventViewModel {
+
+	private ArrayList<EvenementSportif> events = new ArrayList<EvenementSportif>();
+	
+	/*public list<EvenementSportif> getAllEvents(){
+		return event.getAllEvents();
+	}*/
+}
