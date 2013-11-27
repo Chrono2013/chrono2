f3212a4bb2a73ab44ced196164ecb4f0ff5fbea6
12012336 <12012336@e010m04.istic.univ-rennes1.fr>
2013-11-27 13:38:06 +0100

    yy

--- chrono3/src/Ctl/MenuCtlMVC.java
+++ chrono3/src/Ctl/MenuCtlMVC.java
@@ -1,5 +1,67 @@
 package Ctl;
 
-public class MenuCtlMVC {
+
+
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
+import org.zkoss.zk.ui.select.annotation.Listen;
+import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.zul.Borderlayout;
+import org.zkoss.zul.Button;
+import org.zkoss.zul.Combobox;
+import org.zkoss.zul.East;
+import org.zkoss.zul.Label;
+import org.zkoss.zul.Timer;
+import org.zkoss.zul.Window;
+
+public class MenuCtlMVC  extends SelectorComposer<Component>{
+/**
+* 
+*/
+private static final long serialVersionUID = 1123933795452948122L;
+
+@Wire
+Button btnDepartCourse;
+
+
+
+@Wire
+Button topin;
+@Wire
+Timer timer;
+@Wire
+Window gererWindow;
+public void doAfterCompose(Component comp) {
+ 
+try {
+super.doAfterCompose(comp);
+} catch (Exception e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+}
+
+gererWindow.setVisible(false);
+  }
+ 
+ 
+@Listen("onClick = #btnDepartCourse")
+   public void departCourse(){
+btnDepartCourse.setLabel("Arreter Course");
+   }
+ 
+@Listen("onClick = #gerer")
+public void afficheGerer(){
+gererWindow.setVisible(!gererWindow.isVisible());
+ 
+
+ 
+   }
+
 
 }
+
