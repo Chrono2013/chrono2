1ae195da3f4ed0390a5ed2ef08f3a884cd48d50d
12012336 <12012336@i206m04.istic.univ-rennes1.fr>
2013-11-28 12:29:01 +0100

    sss

--- chrono3/src/Ctl/VoitureConcurrentCtlMVC.java
+++ chrono3/src/Ctl/VoitureConcurrentCtlMVC.java
@@ -1,5 +1,17 @@
 package Ctl;
 
-public class VoitureConcurrentCtlMVC {
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.select.SelectorComposer;
 
+public class VoitureConcurrentCtlMVC extends SelectorComposer<Component>{
+	public void doAfterCompose(Component comp) {
+		 
+		try {
+		super.doAfterCompose(comp);
+		} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+		
+  }
 }
